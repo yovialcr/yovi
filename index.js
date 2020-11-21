069e464accb6e5e16e6a0d51158d148d3a48a83a
yovialcr <74792116+yovialcr@users.noreply.github.com>
2020-11-21 11:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'JARVIS'; 
+const instagram = 'https://instagram.com/xenxasin'; 
+const telegram = 'https://t.me/yovi'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/XENxaxin';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
